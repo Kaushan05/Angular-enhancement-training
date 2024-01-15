f61233dc8a929a74aeaefa39be776c2e2a85ea9a
Kaushan05 <kaushan27052001@gmail.com>
2024-01-15 22:01:12 +0530

    logo updated

---
+++ src/app/shared-module/Services/credentials.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CredentialsService {
+  constructor(private http: HttpClient) {}
+
+  enviornment: string = 'http://localhost:3000';
+
+  addAttendance(post_attendance_data: any) {
+    return this.http.post(
+      this.enviornment + '/post_attendance_data',
+      post_attendance_data
+    );
+  }
+}
