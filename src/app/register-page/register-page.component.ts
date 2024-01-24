722d931dea8f6fb7f3ad6c7bde7c249a46e4b20e
Kaushan05 <kaushan27052001@gmail.com>
2024-01-24 19:16:58 +0530

    Register page ui done

--- src/app/register-page/register-page.component.ts
+++ src/app/register-page/register-page.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styleUrl: './register-page.component.css'
+  styleUrl: './register-page.component.css',
 })
 export class RegisterPageComponent {
+  UserProfileDetails = new FormGroup({
+    fullname: new FormControl('', [Validators.required]),
+    useremail: new FormControl('', [Validators.required, Validators.email]),
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [
+      (c: AbstractControl) => Validators.required(c),
+      Validators.required,
+      // Validators.minLength(5),
+      Validators.pattern(
+        /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/
+      ),
+    ]),
+    confrimpassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.pattern(
+        /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/
+      ),
+    ]),
+  });
 
+  onSubmit() {
+    console.log(this.UserProfileDetails);
+  }
 }
--- src/app/register-page/register-page.module.ts
+++ src/app/register-page/register-page.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { RegisterPageRoutingModule } from './register-page-routing.module';
 import { RegisterPageComponent } from './register-page.component';
-
+import { SharedModuleModule } from '../shared-module/shared-module.module';
 
 @NgModule({
-  declarations: [
-    RegisterPageComponent
-  ],
-  imports: [
-    CommonModule,
-    RegisterPageRoutingModule
-  ]
+  declarations: [RegisterPageComponent],
+  imports: [CommonModule, RegisterPageRoutingModule, SharedModuleModule],
 })
-export class RegisterPageModule { }
+export class RegisterPageModule {}
