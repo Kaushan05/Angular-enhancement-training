d3dad278e67513a2481c189bb13575d6dc1cd6c7
Kaushan05 <kaushan27052001@gmail.com>
2024-01-30 19:27:21 +0530

    register form posting wrk

--- src/app/register-page/register-page.component.ts
+++ src/app/register-page/register-page.component.ts
@@ -5,13 +5,15 @@ import {
   Validators,
   AbstractControl,
 } from '@angular/forms';
-
+import { CredentialsService } from '../shared-module/Services/credentials.service';
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css',
 })
 export class RegisterPageComponent {
+  constructor(private postLink: CredentialsService) {}
+
   UserProfileDetails = new FormGroup({
     fullname: new FormControl('', [Validators.required]),
     useremail: new FormControl('', [Validators.required, Validators.email]),
@@ -21,19 +23,24 @@ export class RegisterPageComponent {
       Validators.required,
       // Validators.minLength(5),
       Validators.pattern(
-        /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/
+        /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{6,}/
       ),
     ]),
     confrimpassword: new FormControl('', [
       Validators.required,
       Validators.minLength(6),
       Validators.pattern(
-        /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/
+        /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{6,}/
       ),
     ]),
   });
 
   onSubmit() {
-    console.log(this.UserProfileDetails);
+    console.log(this.UserProfileDetails.value);
+    this.postLink
+      .registerUser(this.UserProfileDetails.value)
+      .subscribe((registeredUserData: any) => {
+        this.UserProfileDetails = registeredUserData;
+      });
   }
 }
--- src/app/shared-module/Services/credentials.service.ts
+++ src/app/shared-module/Services/credentials.service.ts
@@ -1,18 +1,25 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class CredentialsService {
   constructor(private http: HttpClient) {}
 
-  enviornment: string = 'http://localhost:3000';
+  enviornment: string = "http://localhost:3000";
 
   addAttendance(post_attendance_data: any) {
     return this.http.post(
-      this.enviornment + '/post_attendance_data',
+      this.enviornment + "/post_attendance_data",
       post_attendance_data
+    );
+  }
+
+  registerUser(DetailsofUser: any) {
+    return this.http.post(
+      this.enviornment + "/RegesteredDetails",
+      DetailsofUser
     );
   }
 }
